1dcedb6189c503f73a6ca29a4d06dc4294eb15c1
Sara María Cerquera <desarrollo@lanuzagroup.com>
2022-11-21 15:51:01 -0500

    parte 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainHeader from "@/components/header/MainHeder";
+import MainProduct from "./components/product/MainProduct";
+
+
+const App = () => {
+    return(<>
+         <MainHeader />
+         <MainProduct />
+    </>)
+}
+export default App;
---
+++ src/components/product/MainProduct.jsx
@@ -0,0 +1,33 @@
+const MainProduct = () => {
+    return (
+        <main>
+        <section>
+             galeria de imagenes 
+        </section>
+        <section>
+             <p>Sneaker Company</p>
+             <h2>Fall Limited Edition Sneakers</h2>
+             <p>
+                These low profile sneakers are you perfect casual wear
+                companion. featuring a durable rubber outer sole,
+                they'll withstand everything the weather can offer.
+             </p>
+             <p>
+                <span>125.00</span>
+                <span>50%</span>
+             </p>
+             <p>250.00</p>
+             <div>
+                <div>
+                    <button>-</button>
+                    <span>0</span>
+                     <button>+</button>
+                </div>
+                <button>Add to Cart</button>
+             </div>
+        </section>
+    </main>
+    );
+};
+
+export default MainProduct
